c4df77c94ea056cb9f27f9e8289fe86b7cf3579b
kris pan <kpan@kriss-MBP.attlocal.net>
2023-09-13 02:25:39 -0500

    shortened blogtitle and reformatted posts

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Kris Pan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'AI Research Engineer with experience in large-scale machine learning algorithm and infrastructure.';
+    : 'Blog for personal insights in AI';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved. My Blog posts do not represent my employer.';
