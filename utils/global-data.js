3a1e8943a6ba6d477fd546913fd3e710c0253185
kris pan <kpan@kriss-MBP.attlocal.net>
2023-09-13 02:19:13 -0500

    deleted example post and changed blog name, title, footertext

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Kris Pan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'AI Research Engineer with experience in large-scale machine learning algorithm and infrastructure.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved. My Blog posts do not represent my employer.';
 
   return {
     name,
