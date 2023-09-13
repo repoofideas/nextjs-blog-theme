38b2d8a3e586af5d7a37e22a88c304d84a3ada8d
kris pan <kpan@kriss-MBP.attlocal.net>
2023-09-13 09:17:05 -0500

    elongated blog title

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Kris Pan';
+    : 'Kris Pan\'s';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Blog for personal insights in AI';
+    : '"AI Perspectives: A Deep Dive into Personal Insights"';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved. My Blog posts do not represent my employer.';
